2a23a7766813dd4bad030c318b49a22633dc60fb
Fan265982 <993773647@qq.comm>
2018-05-24 17:23:07 +0800

    Initial commit

---
+++ src/main/Java/com/test/test.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class test {
+    private String se;
+
+    public String getSe() {
+        return se;
+    }
+
+    public void setSe(String se) {
+        this.se = se;
+    }
+}
